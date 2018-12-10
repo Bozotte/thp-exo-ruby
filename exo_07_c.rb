35a98a14cc2943754e05c2217be37dcf20415493
Shynn <shynn@icloud.com>
2018-12-11 00:52:43 +0100

    Ajouts des derniers exercices

---
+++ exo_07_c.rb
@@ -0,0 +1,4 @@
+user_name = gets.chomp
+puts user_name
+
+# Spécificité : Aucune question posé, impossible de savoir que l'utilisateur doit entrer des infos. Affiche le prénom si indiqué.
---
+++ exo_09.rb
@@ -0,0 +1,5 @@
+puts "Quel est ton prénom jeune abricot ?"
+firstname = gets.chomp
+puts "Et quel est ton nom de famille jeune citron ?"
+lastname = gets.chomp
+puts "Bonjour, #{firstname} #{lastname} !"
