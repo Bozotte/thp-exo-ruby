6647db3d3a81f978907b5c143c7c3566279a12c7
Shynn <shynn@icloud.com>
2018-12-11 23:33:15 +0100

    Optimisation code : suppresion du doublon de la variable temp

--- pyramide.rb
+++ pyramide.rb
@@ -2,11 +2,10 @@ puts "Choisi un nombre entre 1 et 25"
 number = gets.chomp.to_i
 
 i = 1
-temp = number - i
 bloc = '#'
 space = ' '
 number.times do 
+    temp = number - i    
     puts (space * temp) + (bloc * i)
     i+=1
-    temp = number - i
 end
--- pyramide_while.rb
+++ pyramide_while.rb
@@ -2,13 +2,12 @@ puts "Choisi un nombre entre 1 et 25"
 number = gets.chomp.to_i
 
 i = 1
-temp = number - i
 bloc = '#'
 space = ' '
 
 while i < number
+    temp = number - i    
     puts (space * temp) + (bloc * i)
     i+=1
-    temp = number - i
 end
 puts bloc * number
