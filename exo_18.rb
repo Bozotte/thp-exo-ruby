b3536bd9286e8cfa32cfc21ae93b428b2545ab82
Shynn <shynn@icloud.com>
2018-12-28 15:31:24 +0100

    Ajout de commentaires sur les exos 18 et 19

--- exo_18.rb
+++ exo_18.rb
@@ -1,5 +1,7 @@
+# Tableau vide qu'on remplira avec une fonction
 emails = []
 
+# Fonction qui remplit le tableau avec des emails numérotés de 1 à 50
 i = 1
 50.times do
     emails << "jean.dupont.#{i}@email.fr"
--- exo_19.rb
+++ exo_19.rb
@@ -1,9 +1,12 @@
+# Tableau vide qu'on remplira avec une fonction
 emails = []
 
+# Fonction qui remplit le tableau avec des emails numérotés de 1 à 50
 i = 1
 50.times do
     emails << "jean.dupont.#{i}@email.fr"
     i += 1
 end
 
+# Affichage des emails avec un nombre pair
 puts emails.select.each_with_index { |_, i| i.odd? }
